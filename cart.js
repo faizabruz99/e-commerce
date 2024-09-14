483e738291c37bdc4bbeab46c8e3d55ff1eb7a0a
Fairuz <faizabruz99@gmail.com>
2024-09-15 01:07:13 +0600

    added assets, and base js and index files

---
+++ cart.js
@@ -0,0 +1,32 @@
+var products = [
+    {
+        id: 1,
+        name: 'pikachu cup',
+        price: 50,
+        image: './assets/pikachu-cup'
+    },
+    {
+        id: 2,
+        name: 'arizona mug',
+        price: 50,
+        image: './assets/arizona-mug'
+    },
+    {
+        id: 3,
+        name: 'blue-mug',
+        price: 10,
+        image: './assets/blue-mug'
+    },
+    {
+        id: 4,
+        name: 'ghost-mug',
+        price: 50,
+        image: './assets/ghost-mug'
+    },
+    {
+        id: 5,
+        name: 'iron flask coffee mug',
+        price: 100,
+        image: './assets/iron-flask'
+    },
+]
